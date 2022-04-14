722aa574d541d0b43940ba49430451eea110d0b9
yamodah <yassinedahlek@gmail.com>
2022-04-14 10:49:34 -0400

    setting up and linking files

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const canvas = document.querySelector('canvas')
-canvas.width = innerWidth
-canvas.height = innerHeight
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fill();
+  }
+}
+const player = new Player(100, 100, 30, "red");
+player.draw();
+console.log(player);
