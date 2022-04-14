0dd0c8718a9f52dd10d5c8460afefe4006af003a
yamodah <yassinedahlek@gmail.com>
2022-04-14 11:51:32 -0400

    spawning enemies now randomizing location

--- index.js
+++ index.js
@@ -39,19 +39,56 @@ class Projectile {
     this.y += this.velocity.y;
   }
 }
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+  update() {
+    this.draw();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
 
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 const player = new Player(centerX, centerY, 30, "red");
 
 const projectiles = [];
-
+const enemies = [];
+function spawnEnemies() {
+  setInterval(() => {
+    const x = Math.random()* canvas.width;
+    const y = Math.random()* canvas.height;
+    const radius = 30;
+    const color = "blue";
+    const angle = Math.atan2(centerY - y, centerX - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 
@@ -65,3 +102,4 @@ addEventListener("click", (e) => {
 });
 
 animate();
+// spawnEnemies();
