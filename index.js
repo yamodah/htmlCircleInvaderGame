4013ea77c17f3acb7ad8081aa4d42eab829ff86b
yamodah <yassinedahlek@gmail.com>
2022-04-14 11:02:21 -0400

    added projectile class

--- index.js
+++ index.js
@@ -18,8 +18,26 @@ class Player {
     ctx.fill();
   }
 }
+class Projectile{
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity
+      }
+      draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+      }
+}
 const centerX = canvas.width/2
 const centerY = canvas.height/2
 const player = new Player(centerX, centerY, 30, "red");
 player.draw();
 console.log(player);
+window.addEventListener("click",()=>{
+    
+})
